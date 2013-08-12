8fb1a7841fe7aeae3ecc4771442d2950c9c8baff
stevleibelt <stev@leibelt.de>
2013-08-12 22:42:52 +0200

    Finished length

---
+++ examples/source/Example/Validator/LenghtValidator.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-12 
+ */
+
+namespace Example\Validator;
+
+/**
+ * Class LengthValidator
+ *
+ * @package Example\Validator
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-12
+ */
+class LengthValidator extends AbstractTableValidator
+{
+    /**
+     * @return bool
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-12
+     */
+    public function isMet()
+    {
+        $length = $this->table->getLength();
+        $maxLength = 160;
+        $minLength = 70;
+
+        return (($length >= $minLength) && ($length <= $maxLength));
+    }
+}
