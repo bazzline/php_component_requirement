de3697828d7a41c646c4b52c53b9f02085d94c2d
stevleibelt <stev@leibelt.de>
2013-09-27 18:02:44 +0200

    Created first draft of interface implementation

--- source/Net/Bazzline/Component/Requirement/RequirementConfiguration.php
+++ source/Net/Bazzline/Component/Requirement/RequirementConfiguration.php
@@ -7,20 +7,30 @@
 namespace Net\Bazzline\Component\Requirement;
 
 /**
- * Class RequirementConfigurationInterface
+ * Class RequirementConfiguration
  *
  * @package Net\Bazzline\Component\Requirement
  * @author stev leibelt <artodeto@arcor.de>
  * @since 2013-09-27
  */
-interface RequirementConfigurationInterface
+class RequirementConfiguration implements RequirementConfigurationInterface
 {
+    /**
+     * @var bool
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-27
+     */
+    private $disabledReturnValue;
+
     /**
      * @return null|bool
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-09-27
      */
-    public function getDisabledReturnValue();
+    public function getDisabledReturnValue()
+    {
+        return $this->disabledReturnValue;
+    }
 
     /**
      * @param $returnValue
@@ -28,5 +38,10 @@ interface RequirementConfigurationInterface
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-09-27
      */
-    public function setDisabledReturnValue($returnValue);
+    public function setDisabledReturnValue($returnValue)
+    {
+        $this->disabledReturnValue = (bool) $returnValue;
+
+        return $this;
+    }
 }
