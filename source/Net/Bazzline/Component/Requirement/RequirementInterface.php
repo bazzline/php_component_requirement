e5e70e6e93437a288739c2ed2145f6cae5b2686e
stevleibelt <stev@leibelt.de>
2013-09-29 15:21:14 +0200

    Added IsDisabledInterface

--- source/Net/Bazzline/Component/Requirement/RequirementInterface.php
+++ source/Net/Bazzline/Component/Requirement/RequirementInterface.php
@@ -9,7 +9,7 @@ namespace Net\Bazzline\Component\Requirement;
  * @author stev leibelt <artodeto@arcor.de>
  * @since 2013-06-25
  */
-interface RequirementInterface extends IsMetInterface
+interface RequirementInterface extends IsDisabledInterface, IsMetInterface
 {
     /**
      * Adds a condition to the requirement
